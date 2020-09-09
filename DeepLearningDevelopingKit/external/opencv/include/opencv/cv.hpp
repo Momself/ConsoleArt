253dde17f0e635dc3f0c6055770c8e9186d53dda
Momself <101282888+Momself@users.noreply.github.com>
2020-09-09 06:06:58 +0000

    implement feedback

--- DeepLearningDevelopingKit/external/opencv/include/opencv/cv.hpp
+++ DeepLearningDevelopingKit/external/opencv/include/opencv/cv.hpp
@@ -30,44 +30,4 @@
 // This software is provided by the copyright holders and contributors "as is" and
 // any express or implied warranties, including, but not limited to, the implied
 // warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the Intel Corporation or contributors be liable for any direct,
-// indirect, incidental, special, exemplary, or consequential damages
-// (including, but not limited to, procurement of substitute goods or services;
-// loss of use, data, or profits; or business interruption) however caused
-// and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
-//M*/
-
-#ifndef OPENCV_OLD_CV_H
-#define OPENCV_OLD_CV_H
-
-#if defined(_MSC_VER)
-    #define CV_DO_PRAGMA(x) __pragma(x)
-    #define __CVSTR2__(x) #x
-    #define __CVSTR1__(x) __CVSTR2__(x)
-    #define __CVMSVCLOC__ __FILE__ "("__CVSTR1__(__LINE__)") : "
-    #define CV_MSG_PRAGMA(_msg) CV_DO_PRAGMA(message (__CVMSVCLOC__ _msg))
-#elif defined(__GNUC__)
-    #define CV_DO_PRAGMA(x) _Pragma (#x)
-    #define CV_MSG_PRAGMA(_msg) CV_DO_PRAGMA(message (_msg))
-#else
-    #define CV_DO_PRAGMA(x)
-    #define CV_MSG_PRAGMA(_msg)
-#endif
-#define CV_WARNING(x) CV_MSG_PRAGMA("Warning: " #x)
-
-//CV_WARNING("This is a deprecated opencv header provided for compatibility. Please include a header from a corresponding opencv module")
-
-#include "opencv2/core/core_c.h"
-#include "opencv2/imgproc/imgproc_c.h"
-#include "opencv2/photo/photo_c.h"
-#include "opencv2/video/tracking_c.h"
-#include "opencv2/objdetect/objdetect_c.h"
-
-#if !defined(CV_IMPL)
-#define CV_IMPL extern "C"
-#endif //CV_IMPL
-
-#endif // __OPENCV_OLD_CV_H_
+// In no event shall the Intel Corporation or contributors be liable fo
