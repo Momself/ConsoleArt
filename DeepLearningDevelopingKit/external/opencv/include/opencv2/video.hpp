5e7c0ce490ec1b5a2000f619914e6012e00fda16
Momself <101282888+Momself@users.noreply.github.com>
2022-05-11 20:50:18 +0000

    second draft

--- DeepLearningDevelopingKit/external/opencv/include/opencv2/video.hpp
+++ DeepLearningDevelopingKit/external/opencv/include/opencv2/video.hpp
@@ -33,16 +33,4 @@
 // warranties of merchantability and fitness for a particular purpose are disclaimed.
 // In no event shall the Intel Corporation or contributors be liable for any direct,
 // indirect, incidental, special, exemplary, or consequential damages
-// (including, but not limited to, procurement of substitute goods or services;
-// loss of use, data, or profits; or business interruption) however caused
-// and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
-//M*/
-
-#ifdef __OPENCV_BUILD
-#error this is a compatibility header which should not be used inside the OpenCV library
-#endif
-
-#include "opencv2/video.hpp"
+// (including, but not limited to, procurement of substi
