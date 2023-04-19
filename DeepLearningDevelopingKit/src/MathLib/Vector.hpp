45a612282b5e420d94dd1ccf3d978fa80754b6e3
Momself <101282888+Momself@users.noreply.github.com>
2023-04-19 18:51:38 +0000

    Dependencies upgrade.

--- DeepLearningDevelopingKit/src/MathLib/Vector.hpp
+++ DeepLearningDevelopingKit/src/MathLib/Vector.hpp
@@ -214,4 +214,180 @@ namespace MathLib
 					throw unmatched_size();
 				else
 					for (size_t j = 0; j < self.n; j++)
-						temp(j) = self(j) - _oth
+						temp(j) = self(j) - _other(j);
+			}
+			catch (std::exception& except) { ExceptionHandle(except); }
+			return temp;
+		}
+
+		/// Substraction of a Vector and a scalar.
+		/// Substract a scalar to each element in the Vector.
+		Vector<T> operator - (const T & _other) const
+		{
+			const Vector<T> & self = *this;
+			Vector<T> temp(n);
+			for (size_t j = 0; j < self.n; j++)
+				temp(j) = self(j) - _other;
+			return temp;
+		}
+
+		// "-=" operator
+		/// Substract another matrix to this Vector.
+		void operator -= (const Vector<T> & _other)
+		{
+			Vector<T> & self = *this;
+			try
+			{
+				if (self.n != _other.n)
+					throw unmatched_size();
+				else
+					for (size_t j = 0; j < self.n; j++)
+						self(j) = self(j) - _other(j);
+			}
+			catch (std::exception& except) { ExceptionHandle(except); }
+		}
+
+		/// Substract another scalar to this Vector.
+		void operator -= (const T & _other)
+		{
+			Vector<T> & self = *this;
+			for (size_t j = 0; j < self.n; j++)
+				self(j) = self(j) - _other;
+		}
+
+		// "*" operator
+		/// Multiplication of a Vector and a scalar.
+		Vector<T> operator * (const T & _other) const
+		{
+			const Vector<T> & self = *this;
+			Vector<T> temp(n);
+			for (size_t j = 0; j < self.n; j++)
+				temp(j) = self(j) * _other;
+			return temp;
+		}
+
+	private:
+
+		std::vector<T> _data;
+		size_t n;
+
+	};
+}
+
+
+namespace MathLib
+{
+	// Default constructor
+	/// Take no parameters.
+	/// After default constructor and before use the Vector object, Init() should be involked.
+	template<class T>
+	inline Vector<T>::Vector(void)
+	{
+
+	}
+
+	// Constructor (Using Size and Type)
+	/// Specified the size of Vector.
+	template<class T>
+	inline Vector<T>::Vector(const size_t _n, const VectorType _type)
+	{
+		Init(_n, _type);
+	}
+
+	// Constructor (Using given Data)
+	/// Using data from a given pointer, which is pointed to an array, to initialize the Vector.
+	template<class T>
+	inline Vector<T>::Vector(const std::initializer_list<int> & _list)
+	{
+		for (auto iter1 = _list.cbegin(); iter1 != _list.cend(); ++iter1)
+		{
+			for (auto iter2 = (*iter1).cbegin(); iter2 != (*iter1).cend(); ++iter2)
+			{
+				this._data->push_pack(*iter2);
+			}
+		}
+	}
+
+	// Initializing function
+	/// Initializing the Vector after defined by default constructor.
+	template<class T>
+	inline void Vector<T>::Init(const size_t _n, const VectorType _type)
+	{
+		switch (_type)
+		{
+		case VectorType::Zero:
+			for (size_t j = 0; j < _n; j++)
+			{
+				T tempElem = 0.f;
+				_data.push_back(tempElem);
+			}
+			break;
+		case VectorType::Ones:
+			for (size_t j = 0; j < _n; j++)
+			{
+				T tempElem = 1.f;
+				_data.push_back(tempElem);
+			}
+			break;
+		case VectorType::Random:
+			for (size_t j = 0; j < _n; j++)
+			{
+				T tempElem = Random();
+				_data.push_back(tempElem);
+			}
+			break;
+		default:
+			break;
+		}
+		n = _n;
+	}
+
+	// Inner product function
+	/// Calculate the inner product of two Vectors.
+	template<class T>
+	inline T Vector<T>::InnerProduct(const Vector<T>& _first, const Vector<T>& _second)
+	{
+		T temp = 0;
+		if (_first.Size() != _second.Size())
+			return 0;
+		for (size_t i = 0; i < _first.Size(); i++)
+		{
+			temp += _first(i) * _second(i);
+		}
+		return temp;
+	}
+
+	// Dot product function
+	/// Dot product is simply another name of inner product.
+	template<class T>
+	inline T Vector<T>::DotProduct(const Vector<T>& _first, const Vector<T>& _second)
+	{
+		return InnerProduct(_first, _second);
+	}
+
+	// Scalar product function
+	/// Calculate the Scalar product of a Vector and a scalar.
+	template<class T>
+	inline T Vector<T>::ScalarProduct(const T & _first, const Vector<T>& _second)
+	{
+		Vector<T> temp(_second.Size());
+		for (size_t j = 0; j < _second.Size(); j++)
+			temp(j) = _second(j) * _first;
+		return temp;
+	}
+
+	// Size function
+	/// Return the size of the Vector.
+	template<class T>
+	inline const size_t Vector<T>::Size(void) const
+	{
+		return n;
+	}
+
+	// Sum function
+	/// Add up all the element in the Vector.
+	template<class T>
+	T Vector<T>::Sum(void) const
+	{
+		const Vector<T> & self = *this;
+		T Sum
