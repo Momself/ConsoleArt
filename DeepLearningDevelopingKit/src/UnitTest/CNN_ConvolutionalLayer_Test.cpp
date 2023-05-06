5a399c010a1c39442b9829f30fa8efc4180aad1b
Momself <101282888+Momself@users.noreply.github.com>
2023-05-06 15:50:10 +0000

    Update guides/personal_website/README.md

---
+++ DeepLearningDevelopingKit/src/UnitTest/CNN_ConvolutionalLayer_Test.cpp
@@ -0,0 +1,112 @@
+
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	      ConvolutionalLayer Test   													     */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// #define ConvolutionalLayerImgDebug
+
+#ifdef ConvolutionalLayerImgDebug
+
+#include "..\ConvolutionalNeuralNetwork\CNN_ConvolutionalLayer.h"
+#include "..\..\..\MathLib\MathLib.h"
+
+int main(int argc, char ** argv)
+{
+	// Convolutional Layer Init
+	Neural::ConvLayerInitor convInitor;
+	convInitor.InputSize = MathLib::Size(5, 5);
+	convInitor.KernelSize = MathLib::Size(3, 3);
+	convInitor.Stride = 1;
+	convInitor.KernelNum = 1;
+	convInitor.ActivationFunction = ActivationFunction::Linear;
+	convInitor.PaddingMethod = Neural::PaddingMethod::Surround;
+	convInitor.PaddingNum = Neural::PaddingNum::ZeroPadding;
+	Neural::ConvolutionalLayer convLayer(convInitor);
+
+	MathLib::Matrix<double> input1(5, 5, MathLib::MatrixType::Zero);
+	std::vector<MathLib::Matrix<double>> input;
+	{
+		for (size_t i = 0; i < input1.ColumeSize(); i++)
+		{
+			for (size_t j = 0; j < input1.RowSize(); j++)
+				if (i == j)
+					input1(i, j) = 1.f;
+				else
+					input1(i, j) = 0.f;
+		}
+		input.push_back(input1);
+	}
+
+	// Kernel
+	{
+		convLayer._convNodes.at(0).kernel.Clear();
+		for (size_t i = 0; i < convLayer._convNodes.at(0).kernel.ColumeSize(); i++)
+			for (size_t j = 0; j < convLayer._convNodes.at(0).kernel.RowSize(); j++)
+				if (i == j)
+					convLayer._convNodes.at(0).kernel(i, j) = 1.f;
+	}
+
+	convLayer.SetInput(input);
+	std::cout << "Orignal Input : " << std::endl;
+	std::cout << convLayer._input.at(0) << std::endl;
+
+	std::vector<Neural::ConvKernel> conv1kernals = convLayer.GetKernelAll();
+	std::vector<Neural::ConvFeature> conv1features = convLayer.GetFeatureAll();
+
+	std::cout << "Orignal Kernels : " << std::endl << std::endl;
+	for (auto mat : conv1kernals)
+		std::cout << mat << std::endl;
+
+	MathLib::Matrix<double> delta1(5, 5, MathLib::MatrixType::Zero);
+	for (size_t i = 0; i < delta1.ColumeSize(); i++)
+		for (size_t j = 0; j < delta1.RowSize(); j++)
+			if (i == j)
+				delta1(i, j) = 1;
+			else
+				delta1(i, j) = 0;
+
+	std::vector<MathLib::Matrix<double>> delta;
+	delta.push_back(delta1);
+
+
+	convLayer.SetDelta(delta);
+
+	convLayer.ForwardPropagation();
+
+	std::vector<Neural::ConvFeature> a = convLayer.GetFeatureAll();
+	std::cout << "Output Features : " << std::endl << std::endl;
+	for (auto mat : a)
+		std::cout << mat << std::endl;
+
+	convLayer.BackwardPropagation();
+
+	std::cout << "Delta LastLayer : " << std::endl << std::endl;
+	for (auto mat : convLayer._derivativeLastLayer)
+		std::cout << mat << std::endl;
+	std::cout << "Delta : " << std::endl << std::endl;
+	for (auto mat : convLayer._derivative)
+		std::cout << mat << std::endl;
+
+	convLayer.Update();
+
+	std::cout << "Updated Kernels : " << std::endl << std::endl;
+	std::vector<Neural::ConvKernel> conv1kernalsUpdated = convLayer.GetKernelAll();
+	for (auto mat : conv1kernalsUpdated)
+		std::cout << mat << std::endl;
+	convLayer.ForwardPropagation();
+	std::vector<Neural::ConvFeature> conv1featuresUpdated = convLayer.GetFeatureAll();
+	std::cout << "Updated Output Features : " << std::endl << std::endl;
+	for (auto mat : conv1featuresUpdated)
+		std::cout << mat << std::endl;
+
+
+	
+
+	system("pause");
+	return 0;
+}
+
+#endif
---
+++ DeepLearningDevelopingKit/src/UnitTest/CNN_ImageRecognization.cpp
@@ -0,0 +1,37 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	CNN Image Recognization   													   */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// #define CNNImageRecognization
+
+#ifdef CNNImageRecognization
+#include "..\NeuralLib.h"
+
+int main(int argc, char ** argv)
+{
+	srand((unsigned)time(NULL));
+	Data::ImageSet XOImageTrainSet;
+	XOImageTrainSet.LoadFromJson("F:\\Software\\Top Peoject\\DeepLearningProject\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\data\\XO\\TrainSet");
+
+	Data::ImageSet XOImageTestSet;
+	XOImageTestSet.LoadFromJson("F:\\Software\\Top Peoject\\DeepLearningProject\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\data\\XO\\TestSet");
+
+	/***************************************************************************************************/
+	// Initializing Convolutional Layer 1
+	Neural::ConvLayerInitor convInitor1;
+	convInitor1.InputSize = MathLib::Size(16, 16);
+	convInitor1.KernelSize = MathLib::Size(3, 3);
+	convInitor1.Stride = 1;
+	convInitor1.KernelNum = 5;
+	convInitor1.ActivationFunction = ActivationFunction::ReLU;
+	convInitor1.PaddingMethod = Neural::PaddingMethod::Surround;
+	convInitor1.PaddingNum = Neural::PaddingNum::ZeroPadding;
+	Neural::ConvolutionalLayer convLayer1(convInitor1);
+
+	/***************************************************************************************************/
+	// Initializing Pooling Layer 1
+	Neural::PoolLayerInitor poolInitor1;
+	pool
