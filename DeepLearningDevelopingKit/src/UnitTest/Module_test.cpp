cbb6f5dbaa8ab4cc60982be71b4a24a1ad315e6c
Momself <101282888+Momself@users.noreply.github.com>
2023-05-12 14:48:33 +0000

    Update default theme

---
+++ DeepLearningDevelopingKit/src/UnitTest/Module_test.cpp
@@ -0,0 +1,47 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	          Module Test   	                                                              */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// #define ModuleDebug
+#include <ctime>
+
+#ifdef ModuleDebug
+#include "..\Algorithm\NeuralNetwork\Module.h"
+
+using namespace Neural;
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	BPNetInitor classicBPN;
+	classicBPN.InputNodeNum = 2;
+	classicBPN.HiddenLayerNum = 1;
+	classicBPN.HiddenNodeNum = 10;
+	classicBPN.OutputNodeNum = 1;
+	classicBPN.InputLayerActivationFunction = ActivationFunction::Sigmoid;
+	classicBPN.HiddenLayerActivationFunction = ActivationFunction::ReLU;
+	classicBPN.LossFunction = LossFunction::MES;
+
+	BPNet TestBP1(classicBPN);
+
+	std::string openPath = "F:\\Software\\Top Peoject\\DeepLearningProject\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\data\\exmaple\\XOR.json";
+
+	NumericSet trainSet;
+	trainSet.LoadFromJson(openPath);
+
+	TestBP1.SetTrainSet(&trainSet);
+	TestBP1.SetTestSet(&trainSet);
+
+	TestBP1.Train();
+	TestBP1.Test();
+
+	system("pause");
+	return 0;
+}
+
+#endif // ModuleDebug
---
+++ DeepLearningDevelopingKit/src/UnitTest/OpenCV_test.cpp
@@ -0,0 +1,21 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	         OpenCV Test   															  */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// #define OpenCVDebug
+
+#ifdef OpenCVDebug
+
+#include "..\Visualizer\Visualize.h"
+
+int main(int argc, char ** argv)
+{
+	srand((unsigned)time(NULL));
+	/*
+	cv::Mat imageTest = cv::imread("F:\\Software\\Top Peoject\\DeepLearningProject\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\data\\XO\\Data\\O_0.png");
+	if (!imageTest.data)
+		return -1;
+	cv::resize(image
