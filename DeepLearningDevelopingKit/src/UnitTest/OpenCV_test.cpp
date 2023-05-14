0b6941d712b1692b5f9246ba9080995bd5065476
Momself <101282888+Momself@users.noreply.github.com>
2023-05-14 20:43:03 +0000

    update ruby to 2.4.1

--- DeepLearningDevelopingKit/src/UnitTest/OpenCV_test.cpp
+++ DeepLearningDevelopingKit/src/UnitTest/OpenCV_test.cpp
@@ -18,4 +18,26 @@ int main(int argc, char ** argv)
 	cv::Mat imageTest = cv::imread("F:\\Software\\Top Peoject\\DeepLearningProject\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\data\\XO\\Data\\O_0.png");
 	if (!imageTest.data)
 		return -1;
-	cv::resize(image
+	cv::resize(imageTest, imageTest, cv::Size(500, 500), cv::INTER_LINEAR);
+	cv::namedWindow("data", cv::WINDOW_AUTOSIZE);
+	cv::imshow("data", imageTest);
+	cv::waitKey();
+	*/
+
+	//cv::Mat testMat1(cv::Size(500, 500), CV_32FC1);
+	//MathLib::Matrix<float> testMatrix1(500, 500, MathLib::MatrixType::Random);
+
+	//testMat1 = Visual::OpenCV::Matrix2Mat<float>(testMatrix1);
+	//testMatrix1 = Visual::OpenCV::Mat2Matrix<float>(testMat1);
+	//std::cout << testMatrix1 << std::endl;
+	//std::cout << testMat1 << std::endl;
+
+	// Visual::Plot2D::Plot2DMatrix(testMatrix1);
+	// Visual::Plot2D::PerlinNoiseDemo(800, 800);
+
+	//system("pause");
+	return 0;
+}
+
+#endif // OpenCVDebug
+
---
+++ DeepLearningDevelopingKit/src/UnitTest/Vector_test.cpp
@@ -0,0 +1,91 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	            Vector Test   	                                                          */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+// #define VectorDebug
+
+#ifdef MathStaticDebug
+int main()
+{
+	Matrix<int, 3, 3> A, B, C, D, E;
+
+	A(1, 1) = 6;
+	B(0, 0) = 3;
+	B(1, 1) = 3;
+	B(2, 2) = 3;
+
+	C = A + B;
+	C.PrintToConsole();
+	C = C + 1;
+	C.PrintToConsole();
+	D = A - B;
+	D.PrintToConsole();
+	E = A * B;
+	E.PrintToConsole();
+	int data[3][3] = { { 1,2,3 },{ 4,5,6 },{ 7,8,9 } };
+	Matrix<int, 3, 3> F(data);
+	F.PrintToConsole();
+
+	F = F * 3;
+	F.PrintToConsole();
+
+	Vector<int, 3> a, b, c;
+	a(0) = 3;
+	a(1) = 3;
+	b(0) = 4;
+
+	a.PrintToConsole();
+	b.PrintToConsole();
+
+	a = b;
+
+	(a + b).PrintToConsole();
+
+	system("pause");
+	return 0;
+}
+#endif // MathStaticDebug
+
+#ifdef VectorDebug
+
+// Header files
+#include <iostream>
+#include "..\MathLib\MathLib.h"
+
+using namespace std;
+using namespace MathLib;
+
+int main()
+{
+	/*
+	Vector<int> debug1(6);
+	Vector<int> debug2(3);
+	debug1 - debug2;
+	*/
+
+
+	Vector<int> input;
+	input.Init(5);
+	for (size_t i = 0; i < 5; i++) 
+		input(i) = i;
+
+	Vector<int> weight;
+	weight.Init(5);
+	for (size_t i = 0; i < 5; i++)
+		weight(i) = i;
+
+	int bias = 2, ans = 0;
+
+	input = input * 2;
+
+	cout << input << weight << bias << std::endl;
+
+	ans = Vector<int>::DotProduct(input, weight) + bias;
+	cout << ans << std::endl;
+
+	Vector<double> test(5, VectorType::Random);
+	cout << test<< test.Sum();
+	system("pause");
+	
