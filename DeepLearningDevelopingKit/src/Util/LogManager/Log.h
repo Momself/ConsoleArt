a538a3116f63e3bad8013b9bea878fc657a1d756
Momself <101282888+Momself@users.noreply.github.com>
2023-05-21 16:40:41 +0000

    Delete .keep

--- DeepLearningDevelopingKit/src/Util/LogManager/Log.h
+++ DeepLearningDevelopingKit/src/Util/LogManager/Log.h
@@ -18,4 +18,15 @@ void PrintInfo(void)
 	temp += "Project : Deep Learning Developing Kit\n";
 	temp += "Branch  : Master\n";
 	temp += "Version : Windows-x64-0.1.0-CPU\n";
-	temp += "Copyright 2015-2018 Celestial Tech Inc.\nFor more check www.tianshi
+	temp += "Copyright 2015-2018 Celestial Tech Inc.\nFor more check www.tianshicangxie.com\n";
+	std::cout << temp << std::endl;
+	std::cout << "Current time : ";
+	PrintLocalTime();
+}
+
+void PrintLocalTime(void)
+{
+	SYSTEMTIME sys;
+	GetLocalTime(&sys);
+	printf("%4d/%02d/%02d %02d:%02d:%02d \n", sys.wYear, sys.wMonth, sys.wDay, sys.wHour, sys.wMinute, sys.wSecond);
+}
